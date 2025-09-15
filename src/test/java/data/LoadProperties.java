60d2e000536dce6dc43521eb5fd2d59784c3e168
Ahmad Hesham <ahmedhesham606@icloud.com>
2025-09-15 18:52:01 +0300

    feat: parellel testing using saucelabs

--- src/test/java/data/LoadProperties.java
+++ src/test/java/data/LoadProperties.java
@@ -7,9 +7,13 @@ import java.util.Properties;
 
 public class LoadProperties {
 
-	private static String propertiesPath = System
+	private static String userDataPropertiesPath = System
 			.getProperty("user.dir") + "\\src\\main\\java\\properties\\UserData.properties";
-	public static Properties userData = loadProperty(propertiesPath);
+	public static Properties userData = loadProperty(userDataPropertiesPath);
+
+	private static String saucelabsUserPropertiesPath = System
+			.getProperty("user.dir") + "\\src\\main\\java\\properties\\SaucelabsUser.properties";
+	public static Properties saucelabsUserData = loadProperty(saucelabsUserPropertiesPath);
 
 	private static Properties loadProperty(String path) {
 		Properties property = new Properties();
--- src/test/java/tests/ParallelTestBase.java
+++ src/test/java/tests/ParallelTestBase.java
@@ -3,6 +3,8 @@ package tests;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -14,20 +16,45 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import data.LoadProperties;
 import utilities.Helper;
 
 public class ParallelTestBase {
+
+	public static final String USERNAME = LoadProperties.saucelabsUserData.getProperty("username");
+	public static final String ACCESS_KEY = LoadProperties.saucelabsUserData.getProperty("accessKey");
+	public static final String BUILD = LoadProperties.saucelabsUserData.getProperty("build");
+	public static final String TEST_NAME = LoadProperties.saucelabsUserData.getProperty("testName");
+	public static final String sauceURL = LoadProperties.saucelabsUserData.getProperty("URL");
 
 	public static String baseURL = "https://demo.nopcommerce.com/";
 
 	protected ThreadLocal<RemoteWebDriver> drivers = new ThreadLocal<>();
 
-	@Parameters(value = "browser")
+	@Parameters({ "browser", "usage", "platform" })
 	@BeforeClass
-	public void openDriver(@Optional("chrome") String browser) throws MalformedURLException {
+	public void openDriver(@Optional("chrome") String browser, @Optional("saucelabs") String usage,
+			@Optional("Windows 10") String platform) throws MalformedURLException {
+
 		DesiredCapabilities caps = new DesiredCapabilities();
 		caps.setCapability("browserName", browser);
-		drivers.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps));
+		if (usage.equalsIgnoreCase("saucelabs")) {
+			Map<String, Object> sauceOptions = new HashMap<>();
+			sauceOptions.put("username", USERNAME);
+			sauceOptions.put("accessKey", ACCESS_KEY);
+			sauceOptions.put("build", BUILD);
+			sauceOptions.put("name", TEST_NAME);
+			sauceOptions.put("platform", platform);
+			sauceOptions.put("screenResolution", "1920x1080");
+
+			caps.setCapability("sauce:options", sauceOptions);
+			URL url = new URL(sauceURL);
+			drivers.set(new RemoteWebDriver(url, caps));
+
+		} else {
+			drivers.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps));
+		}
+
 		getDriver().get(baseURL);
 	}
 
@@ -38,12 +65,12 @@ public class ParallelTestBase {
 	@AfterClass
 	public void stopDriver() {
 		RemoteWebDriver driver = (RemoteWebDriver) getDriver();
-	    if (driver != null) {
-	        driver.quit();
-	        drivers.remove();
-	    }
+		if (driver != null) {
+			driver.quit();
+			drivers.remove();
+		}
 	}
-	
+
 	@AfterMethod
 	public void takeScreenshotOnFailure(ITestResult methodResult) throws IOException {
 		if (methodResult.getStatus() == ITestResult.FAILURE) {
